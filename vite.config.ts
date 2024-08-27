8f9da885351243f868306d33eb911a1944352d28
huangzhenye <vongzain.bo@gmail.com>
2024-08-27 16:59:02 +0800

    feat: husky配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,22 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		port: 8080,
-		host: 'localhost',
-		open: true,
-		proxy: {
-			'/api': {
-				target: 'http://api-driver.marsview.cc',
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, '')
-			}
-		}
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	}
+  server: {
+    port: 8080,
+    host: 'localhost',
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://api-driver.marsview.cc',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
