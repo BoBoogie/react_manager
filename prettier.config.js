94f596c2914525da027c115151efc5311860e07e
huangzhenye <vongzain.bo@gmail.com>
2024-08-27 16:15:54 +0800

    feat: editconfig、prettier、eslint、vite配置完成

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+// 配置代码格式化工具的选项
+module.exports = {
+	// 在语句结尾添加分号
+	semi: false,
+	// 添加尾随逗号
+	trailingComma: 'all',
+	// 使用单引号
+	singleQuote: true,
+	// 在jsx中使用单引号
+	jsxSingleQuote: true,
+	// 每行最大字符数
+	printWidth: 120,
+	// 缩进时每个层级使用的空格数
+	tabWidth: 2,
+	// 使用制表符而不是空格缩进
+	useTabs: false,
+	// 对象字面量的大括号之间留空格
+	bracketSpacing: true,
+	// JSX 的闭合标签与前一个标签在同一行
+	jsxBracketSameLine: false,
+	// 箭头函数参数避免不必要的括号包裹
+	arrowParens: 'avoid',
+	// 文件的换行符
+	endOfLine: 'lf'
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	server: {
+		port: 8080,
+		host: 'localhost',
+		open: true,
+		proxy: {
+			'/api': {
+				target: 'http://api-driver.marsview.cc',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	}
 })
