54bf2ea5bfbdc7c0bdcbb8dbaaa5e714d3b4d7a7
huangzhenye <vongzain.bo@gmail.com>
2024-08-30 12:04:34 +0800

    feat: 代码格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,14 +12,14 @@ export default defineConfig({
       '/api': {
         target: 'http://api-driver.marsview.cc',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
