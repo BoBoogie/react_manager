74f7995a24e2546d97f0eb0566418361f3d55b50
huangzhenye <vongzain.bo@gmail.com>
2024-09-03 17:17:20 +0800

    feat: 主题配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router/index';
+import { ConfigProvider } from 'antd';
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#9D68D3'
+        }
+      }}>
       <RouterProvider router={router} />
-    </>
+    </ConfigProvider>
   );
 }
 
