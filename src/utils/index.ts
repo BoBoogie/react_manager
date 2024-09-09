4bef6b474970bbed16e951dc838a53bdb4ff7726
huangzhenye <vongzain.bo@gmail.com>
2024-09-09 12:12:43 +0800

    fix: 工具函数修复

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,16 +3,19 @@
  */
 
 // 格式化金额
-export const formatMoney = (num: number | string) => {
+export const formatMoney = (num?: number | string) => {
+  if (!num) return '0.00';
   const a = parseFloat(num.toString());
   return a.toLocaleString('zh-CN', { style: 'currency', currency: 'CNY' });
 };
 
 // 格式化数字
-export const formatNum = (num: number | string) => {
+export const formatNum = (num?: number | string) => {
+  if (!num) return '0';
+
   const a = num.toString();
-  if (a.indexOf('.') > -1) return a.replace(/{\d}(?=(\d{3})+\.)/g, '$1,');
-  return a.replace(/{\d}(?=(\d{3})+$)/g, '$1,');
+  if (a.indexOf('.') > -1) return a.replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+  return a.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
 };
 
 // 格式化日期
